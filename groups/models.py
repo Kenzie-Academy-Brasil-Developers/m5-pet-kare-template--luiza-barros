c1c34ff5cf91d9dcb9d417b1253906cc6318747e
Luiza Barros <thecoderana@gmail.com>
2023-02-21 17:58:13 -0300

    fix: foreign key not null error

--- groups/models.py
+++ groups/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Group(models.Model):
     scientific_name = models.CharField(max_length=50, unique=True)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     pet = models.ForeignKey(
-        "pets.Pet", on_delete=models.RESTRICT, related_name="group"
+        "pets.Pet",
+        on_delete=models.RESTRICT,
+        related_name="group",
+        blank=True,
+        null=True,
     )
